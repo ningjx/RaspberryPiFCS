6c4052cefc0b4c942db21d24719bc68c8d9516c2
wangning <wangning@beisen.com>
2019-08-29 11:29:32 +0800

    优化驱动

--- RaspberryPiFMS/Helper/Pca9685Helper.cs
+++ RaspberryPiFMS/Helper/Pca9685Helper.cs
@@ -9,59 +9,29 @@ namespace RaspberryPiFMS.Helper
 {
     public class Pca9685
     {
-        int SUBADR1 = 0x02;
-        int SUBADR2 = 0x03;
-        int SUBADR3 = 0x04;
-        int MODE1 = 0x00;
-        int PRESCALE = 0xFE;
-        int LED0_ON_L = 0x06;
-        int LED0_ON_H = 0x07;
-        int LED0_OFF_L = 0x08;
-        int LED0_OFF_H = 0x09;
-        int ALLLED_ON_L = 0xFA;
-        int ALLLED_ON_H = 0xFB;
-        int ALLLED_OFF_L = 0xFC;
-        int ALLLED_OFF_H = 0xFD;
+        #region 变量
+        const int SUBADR1 = 0x02;
+        const int SUBADR2 = 0x03;
+        const int SUBADR3 = 0x04;
+        const int MODE1 = 0x00;
+        const int PRESCALE = 0xFE;
+        const int LED0_ON_L = 0x06;
+        const int LED0_ON_H = 0x07;
+        const int LED0_OFF_L = 0x08;
+        const int LED0_OFF_H = 0x09;
+        const int ALLLED_ON_L = 0xFA;
+        const int ALLLED_ON_H = 0xFB;
+        const int ALLLED_OFF_L = 0xFC;
+        const int ALLLED_OFF_H = 0xFD;
 
         private II2CDevice device;
-
-        public Pca9685(int addr = 0x40)
+        #endregion
+        public Pca9685(int addr = 0x40, double freq = 50)
         {
             I2CBus bus = new I2CBus();
             device = bus.AddDevice(addr);
             device.WriteAddressByte(MODE1, 0x00);
-        }
-
-        private void Write(int reg, byte value)
-        {
-            device.WriteAddressByte(reg, value);
-        }
-
-        private byte Read(int reg)
-        {
-            return device.ReadAddressByte(reg);
-        }
-
-        /// <summary>
-        /// 设置PWM频率
-        /// </summary>
-        /// <param name="freq"></param>
-        public void SetPWMFreq(double freq)
-        {
-            double prescaleval = 25000000.0;   // 25MHz
-            prescaleval /= 4096.0;       // 12-bit
-            prescaleval /= (float)freq;
-            prescaleval -= 1.0;
-
-            var prescale = Math.Floor(prescaleval + 0.5);
-
-            var oldmode = Read(MODE1);
-            var newmode = (oldmode & 0x7F) | 0x10;       // sleep
-            Write(MODE1, Convert.ToByte(newmode));      // go to sleep
-            Write(PRESCALE, Convert.ToByte(Math.Floor(prescale)));
-            Write(MODE1, oldmode);
-            Thread.Sleep(5);
-            Write(MODE1, Convert.ToByte(oldmode | 0x80));
+            SetPWMFreq(freq);
         }
 
         /// <summary>
@@ -76,6 +46,38 @@ namespace RaspberryPiFMS.Helper
             Write(LED0_ON_H + 4 * channel, 0 >> 8);
             Write(LED0_OFF_L + 4 * channel, Convert.ToByte(off & 0xFF));
             Write(LED0_OFF_H + 4 * channel, Convert.ToByte(off >> 8));
+        }
+
+        /// <summary>
+        /// 设置PWM频率
+        /// </summary>
+        /// <param name="freq"></param>
+        private void SetPWMFreq(double freq)
+        {
+            double prescaleval = 25000000.0;   // 25MHz
+            prescaleval /= 4096.0;       // 12-bit
+            prescaleval /= (float)freq;
+            prescaleval -= 1.0;
+
+            var prescale = Math.Floor(prescaleval + 0.5);
+
+            var oldmode = Read(MODE1);
+            var newmode = (oldmode & 0x7F) | 0x10;
+            Write(MODE1, Convert.ToByte(newmode));
+            Write(PRESCALE, Convert.ToByte(Math.Floor(prescale)));
+            Write(MODE1, oldmode);
+            Thread.Sleep(5);
+            Write(MODE1, Convert.ToByte(oldmode | 0x80));
+        }
+
+        private void Write(int reg, byte value)
+        {
+            device.WriteAddressByte(reg, value);
+        }
+
+        private byte Read(int reg)
+        {
+            return device.ReadAddressByte(reg);
         }
 
         private int ConvertAngle(double angle)
---
+++ RaspberryPiFMSTest/HelperTest/Pca9685HelperTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaspberryPiFMS.Helper;
+
+namespace RaspberryPiFMSTest.HelperTest
+{
+    [TestClass]
+    public class Pca9685HelperTest
+    {
+        [TestMethod]
+        public void Pca9685Test()
+        {
+            Pca9685 pca9685 = new Pca9685();
+            pca9685.SetPWMAngle(0,10);
+        }
+    }
+}
