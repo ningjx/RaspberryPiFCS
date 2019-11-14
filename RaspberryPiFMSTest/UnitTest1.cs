3ccb5367f7083a6971d5fc0349ccb15efd393c14
wangning <wangning@beisen.com>
2019-11-14 18:32:32 +0800

    单测

--- RaspberryPiFMSTest/UnitTest1.cs
+++ RaspberryPiFMSTest/UnitTest1.cs
@@ -1,20 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaspberryPiFMS.Helper;
 using System.Diagnostics;
 using System.IO.Ports;
+using System.Threading;
+
 namespace RaspberryPiFMSTest
 {
     [TestClass]
     public class UnitTest1
     {
+        Stopwatch sw = new Stopwatch();
         [TestMethod]
         public void TestMethod1()
         {
-            Stopwatch sw = new Stopwatch();
+            MicroTimer timer = new MicroTimer();
+            timer.Interval = 1000;
+            timer.AutoReset = false;
+            timer.Elapsed += Stop;
             sw.Start();
-            while (true)
-            {
-                var aa = sw.Elapsed.TotalMilliseconds;
-            }
+            timer.Start();
+            Thread.Sleep(1200);
+            var a = sw.Elapsed.Milliseconds;
+        }
+
+        private void Stop()
+        {
+            sw.Stop();
         }
     }
 }
