0703b625e87fe39e9b606e61d934670514a71771
王宁 <vipning@qq.com>
2020-03-25 19:17:39 +0800

    添加B737PFD仪表

--- RaspberryPiClient/MainForm.Designer.cs
+++ RaspberryPiClient/MainForm.Designer.cs
@@ -29,181 +29,40 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.GMapControl = new GMap.NET.WindowsForms.GMapControl();
-            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.attitudeIndicatorInstrumentControl1 = new RaspberryPiClient.CustomControls.AttitudeIndicatorInstrumentControl();
+            this.b737PFD1 = new RaspberryPiClient.MyPannel.B737PFD.B737PFD();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.altimeterInstrumentControl1 = new RaspberryPiClient.CustomControls.AltimeterInstrumentControl();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.verticalSpeedIndicatorInstrumentControl1 = new RaspberryPiClient.CustomControls.VerticalSpeedIndicatorInstrumentControl();
-            this.headingIndicatorInstrumentControl1 = new RaspberryPiClient.CustomControls.HeadingIndicatorInstrumentControl();
             this.SuspendLayout();
             // 
-            // GMapControl
+            // b737PFD1
             // 
-            this.GMapControl.Bearing = 0F;
-            this.GMapControl.CanDragMap = true;
-            this.GMapControl.EmptyTileColor = System.Drawing.Color.Navy;
-            this.GMapControl.GrayScaleMode = false;
-            this.GMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.GMapControl.LevelsKeepInMemmory = 5;
-            this.GMapControl.Location = new System.Drawing.Point(0, 0);
-            this.GMapControl.MarkersEnabled = true;
-            this.GMapControl.MaxZoom = 2;
-            this.GMapControl.MinZoom = 2;
-            this.GMapControl.MouseWheelZoomEnabled = true;
-            this.GMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            this.GMapControl.Name = "GMapControl";
-            this.GMapControl.NegativeMode = false;
-            this.GMapControl.PolygonsEnabled = true;
-            this.GMapControl.RetryLoadTile = 0;
-            this.GMapControl.RoutesEnabled = true;
-            this.GMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
-            this.GMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
-            this.GMapControl.ShowTileGridLines = false;
-            this.GMapControl.Size = new System.Drawing.Size(150, 150);
-            this.GMapControl.TabIndex = 0;
-            this.GMapControl.Zoom = 0D;
-            // 
-            // gMapControl1
-            // 
-            this.gMapControl1.Bearing = 0F;
-            this.gMapControl1.CanDragMap = true;
-            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
-            this.gMapControl1.GrayScaleMode = false;
-            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
-            this.gMapControl1.MarkersEnabled = true;
-            this.gMapControl1.MaxZoom = 2;
-            this.gMapControl1.MinZoom = 2;
-            this.gMapControl1.MouseWheelZoomEnabled = true;
-            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            this.gMapControl1.Name = "gMapControl1";
-            this.gMapControl1.NegativeMode = false;
-            this.gMapControl1.PolygonsEnabled = true;
-            this.gMapControl1.RetryLoadTile = 0;
-            this.gMapControl1.RoutesEnabled = true;
-            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
-            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
-            this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(396, 263);
-            this.gMapControl1.TabIndex = 0;
-            this.gMapControl1.Zoom = 0D;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // attitudeIndicatorInstrumentControl1
-            // 
-            this.attitudeIndicatorInstrumentControl1.Location = new System.Drawing.Point(402, 31);
-            this.attitudeIndicatorInstrumentControl1.Name = "attitudeIndicatorInstrumentControl1";
-            this.attitudeIndicatorInstrumentControl1.Size = new System.Drawing.Size(535, 530);
-            this.attitudeIndicatorInstrumentControl1.TabIndex = 1;
-            this.attitudeIndicatorInstrumentControl1.Text = "attitudeIndicatorInstrumentControl1";
+            this.b737PFD1.Location = new System.Drawing.Point(144, 49);
+            this.b737PFD1.Name = "b737PFD1";
+            this.b737PFD1.Size = new System.Drawing.Size(603, 553);
+            this.b737PFD1.TabIndex = 0;
+            this.b737PFD1.Text = "b737PFD1";
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // altimeterInstrumentControl1
-            // 
-            this.altimeterInstrumentControl1.Location = new System.Drawing.Point(1011, 331);
-            this.altimeterInstrumentControl1.Name = "altimeterInstrumentControl1";
-            this.altimeterInstrumentControl1.Size = new System.Drawing.Size(292, 307);
-            this.altimeterInstrumentControl1.TabIndex = 2;
-            this.altimeterInstrumentControl1.Text = "altimeterInstrumentControl1";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(0, 0);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(78, 16);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(412, 644);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(644, 644);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(1011, 644);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 21);
-            this.textBox3.TabIndex = 6;
-            // 
-            // verticalSpeedIndicatorInstrumentControl1
-            // 
-            this.verticalSpeedIndicatorInstrumentControl1.Location = new System.Drawing.Point(146, 390);
-            this.verticalSpeedIndicatorInstrumentControl1.Name = "verticalSpeedIndicatorInstrumentControl1";
-            this.verticalSpeedIndicatorInstrumentControl1.Size = new System.Drawing.Size(198, 207);
-            this.verticalSpeedIndicatorInstrumentControl1.TabIndex = 7;
-            this.verticalSpeedIndicatorInstrumentControl1.Text = "verticalSpeedIndicatorInstrumentControl1";
-            this.verticalSpeedIndicatorInstrumentControl1.Click += new System.EventHandler(this.verticalSpeedIndicatorInstrumentControl1_Click);
-            // 
-            // headingIndicatorInstrumentControl1
-            // 
-            this.headingIndicatorInstrumentControl1.Location = new System.Drawing.Point(999, 31);
-            this.headingIndicatorInstrumentControl1.Name = "headingIndicatorInstrumentControl1";
-            this.headingIndicatorInstrumentControl1.Size = new System.Drawing.Size(249, 256);
-            this.headingIndicatorInstrumentControl1.TabIndex = 8;
-            this.headingIndicatorInstrumentControl1.Text = "headingIndicatorInstrumentControl1";
+            this.timer1.Tick += new System.EventHandler(this.MainForm_Load);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1388, 715);
-            this.Controls.Add(this.headingIndicatorInstrumentControl1);
-            this.Controls.Add(this.verticalSpeedIndicatorInstrumentControl1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.altimeterInstrumentControl1);
-            this.Controls.Add(this.attitudeIndicatorInstrumentControl1);
-            this.Controls.Add(this.gMapControl1);
+            this.ClientSize = new System.Drawing.Size(912, 664);
+            this.Controls.Add(this.b737PFD1);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private GMap.NET.WindowsForms.GMapControl GMapControl;
-        private GMap.NET.WindowsForms.GMapControl gMapControl1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private CustomControls.AttitudeIndicatorInstrumentControl attitudeIndicatorInstrumentControl1;
+
+        private MyPannel.B737PFD.B737PFD b737PFD1;
         private System.Windows.Forms.Timer timer1;
-        private CustomControls.AltimeterInstrumentControl altimeterInstrumentControl1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private CustomControls.VerticalSpeedIndicatorInstrumentControl verticalSpeedIndicatorInstrumentControl1;
-        private CustomControls.HeadingIndicatorInstrumentControl headingIndicatorInstrumentControl1;
     }
-
-
 }
--- RaspberryPiClient/MainForm.cs
+++ RaspberryPiClient/MainForm.cs
@@ -8,51 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FlightDataModel;
-using GMap.NET;
-using GMap.NET.WindowsForms;
 using RaspberryPiClient.Controllers;
-using RaspberryPiClient.Helper;
-using SharpGL.WinForms;
+
 namespace RaspberryPiClient
 {
-    public partial class MainForm : Form 
+    public partial class MainForm : Form
     {
-        FlightData data;
-        int a = 0;
+        FlightData data = new FlightData();
         public MainForm()
         {
             InitializeComponent();
-            gMapControl1.MapProvider = AMapProvider.Instance;
-            GMaps.Instance.Mode = AccessMode.ServerAndCache;
-            gMapControl1.Zoom = 10;
-            gMapControl1.MinZoom = 1;
-            gMapControl1.MaxZoom = 24;//指定最大最小zoom才可以缩放
-            gMapControl1.DragButton = MouseButtons.Left;
-            //gMapControl1.Position = new PointLatLng(100, 100);
-            gMapControl1.SetPositionByKeywords("北京");
             data = TestEq.FlightData;
-            timer1.Start();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            textBox1.Text =Convert.ToString(180-data.Attitude.Angle_Y);
-            textBox2.Text =Convert.ToString( data.Attitude.Angle_X - 180);
-            textBox3.Text =Convert.ToString((int)data.Attitude.BarometricAltitude);
-            attitudeIndicatorInstrumentControl1.SetAttitudeIndicatorParameters(180-data.Attitude.Angle_Y, data.Attitude.Angle_X-180);
-            altimeterInstrumentControl1.SetAlimeterParameters(a++);
-            verticalSpeedIndicatorInstrumentControl1.SetVerticalSpeedIndicatorParameters((int)data.Attitude.Aacceleration_Y*1000);
-            headingIndicatorInstrumentControl1.SetHeadingIndicatorParameters((int)data.Attitude.Angle_Z);
-        }
-
-        private void verticalSpeedIndicatorInstrumentControl1_Click(object sender, EventArgs e)
-        {
-
+            b737PFD1.SetAttitudeIndicatorParameters(data.Attitude.Angle_X - 180, 180 - data.Attitude.Angle_Y, data.Attitude.BarometricAltitude, data.Attitude.Aacceleration_X, 0, data.Attitude.Angle_Z);
         }
     }
 }
