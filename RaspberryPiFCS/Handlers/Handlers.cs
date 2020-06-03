d4b66b8cecfc45d92a1d3b5592ac80697b0e2230
wangning <vipning@qq.com>
2020-06-03 09:53:07 +0800

    修改新的UARTDriver

--- RaspberryPiFCS/Handlers/Handlers.cs
+++ RaspberryPiFCS/Handlers/Handlers.cs
@@ -6,4 +6,5 @@ namespace RaspberryPiFCS
     public delegate void DataHandler(byte[] bytes);
     public delegate void WatcherHandler();
     public delegate void ErrorMessageHandler(ErrorType errorType, string message, Exception ex);
+    public delegate void UARTRecHandler(byte[] bytes);
 }
